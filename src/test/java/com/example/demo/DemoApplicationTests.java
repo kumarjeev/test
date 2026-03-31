1085200f12fa5628cacb051b30c35457770b41dc
kumarjeev <THATZJEEVANKUMAR@GMAIL.COM>
2026-03-31 22:37:15 +0530

    Update DemoApplicationTests.java
    added

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -5,7 +5,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
-
+	int x=80;
+	int y=45;
+	boolean isSub=true;
 	@Test
 	void contextLoads() {
 	}
